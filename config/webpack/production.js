d6b3e485a2a4ff40c1cb898df1d1745ec7008453
Samuelson Okoi <samuelsonokoi@gmail.com>
2021-05-26 13:39:56 +0100

    modified config/webpack/production.js to include environment.js in export

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -4,4 +4,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig();
+module.exports = environment.toWebpackConfig(environment);
+// module.exports = environment.toWebpackConfig();
+
+// const { webpackConfig, merge } = require('@rails/webpacker')
+// const customConfig = require('./custom')
+
+// module.exports = merge(webpackConfig, customConfig)
